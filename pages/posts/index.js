33fb3b443a450d11b4d371f7c2ff3bfb6d12a184
antuncrnja <antuncrnja@gmail.com>
2020-09-03 20:06:52 +0200

    update

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -4,22 +4,25 @@ import Container from '../../components/Container'
 import Flex from '../../components/Flex'
 import { url } from '../api/url'
 import { useState } from 'react'
+import Head from 'next/head'
+
 
 
 
 const Posts = ({ posts, allPosts, lastPost }) => {
 	let showMore = 2;
+	
 	const [load, setLoad] = useState(showMore)
-
 	
 	const loader = () => {
 		const test = 1
-		lastPost = posts[load - test].title.rendered;
 
+		lastPost = posts[load - test].title.rendered;
+		
 		const allA = document.querySelectorAll('.test')
 		allA.forEach( x => {
 			if(x.querySelector('p').innerText == lastPost){
-				x.setAttribute("id", "zadnji");
+				x.setAttribute("id", "scroll");
 				console.log(load)
 				console.log(lastPost)
 			}else{
@@ -33,26 +36,38 @@ const Posts = ({ posts, allPosts, lastPost }) => {
 	}
    
 	return(
-	  <Container>
-		  <Flex>
-				{posts.slice(0,load).map((post) => (
-					<Card key={post.id} >
-					<Link href={ `/blog/${ post.slug }` }>
-						<a href={ `/blog/${ post.slug }`} className="test">
-				<p>{post.title.rendered}</p>
-							<img src={
-								post.better_featured_image 
-								? post.better_featured_image.media_details.sizes.medium.source_url 
-								: 'https://www.ilac.com/wp-content/uploads/2019/06/placeholder-600x400.png'} />
-						</a>
-					</Link>
-					</Card>
-				))}
-             
-			 
-		</Flex>
-		<a href="#zadnji"><button onClick={() => setLoad(loader)} className={load >= allPosts ? 'erase' : 'active'}>LOAD MORE</button></a>
-	  </Container>
+		<>
+		<Head>
+			<title>Posts</title>
+		</Head>
+			<Container>
+				<Flex>
+						{posts.slice(0,load).map((post) => (
+							<Card key={post.id} >
+							<Link href={ `/blog/${ post.slug }` }>
+								<a href={ `/blog/${ post.slug }`} className="test">
+						<p>{post.title.rendered}</p>
+									<img src={
+										post.better_featured_image 
+										? post.better_featured_image.media_details.sizes.medium.source_url 
+										: 'https://www.ilac.com/wp-content/uploads/2019/06/placeholder-600x400.png'} />
+								</a>
+							</Link>
+							</Card>
+						))}
+					
+					
+				</Flex>
+				<a href="#scroll"><button onClick={() => setLoad(loader)} className={load >= allPosts ? 'erase' : 'active'}>LOAD MORE</button></a>
+			</Container>
+			<style global jsx>
+   {`
+       html {
+          scroll-behavior: smooth;
+        }
+  `}
+  </style>
+	  </>
 )
 }
 
