7a3d9a760b16c87b81df2699f1f62b7c2f418945
Thoonx <ggold.zg@gmail.com>
2020-12-31 20:44:32 +0100

    update

--- pages/test.js
+++ pages/test.js
@@ -9,34 +9,41 @@ import { useRouter } from 'next/router'
 import { useEffect } from 'react'
 
 const Posts = ({ posts }) => {
+const router = useRouter();
 
 	const [myPosts, setMyPosts] = useState(posts);
 
 	const [activeFilter, setActiveFilter] = useState({
 		title: '',
-		title2: ''
+		title2: '',
+		title3: ''
 	})
 
 	useEffect(() => {
 		setMyPosts( 
 			posts.filter( post => post.title.toLowerCase().includes(activeFilter.title) 
 			   && post.title.toLowerCase().includes(activeFilter.title2)
+			   && post.title.toLowerCase().includes(activeFilter.title3)
 		    )
 		)
+		
 	}, [activeFilter])
 
 		function handleOption(e){
-			if(e.target.id == 'option'){
+			e.target.id == 'option' &&
 				setActiveFilter({...activeFilter, title: e.target.value.toLowerCase()})
-			}
-			if(e.target.id == 'option2'){
+			e.target.id == 'option2' &&
 				setActiveFilter({...activeFilter, title2: e.target.value.toLowerCase()})
-			}
 		}
-
-		useEffect( () => {
-			console.log(activeFilter)
-		},[activeFilter])
+function runForm(e){
+	setActiveFilter({
+		title2: e.target.test2.value.toLowerCase(), 
+		title: e.target.test.value.toLowerCase(),
+		title3: e.target.test3.checked ? e.target.test3.value.toLowerCase() : ""
+	})
+	console.log(activeFilter)
+	e.preventDefault();
+}
 	
 	return(
 		<>
@@ -45,6 +52,15 @@ const Posts = ({ posts }) => {
 		</Head>
 			<Container>
 			<Flex>
+				<form onSubmit={runForm}>
+					<input type="text" name="test" id="" defaultValue="new" placeholder="Search posts"/>
+					<input type="text" name="test2" id="" defaultValue="post" placeholder="Search posts"/>
+						<label>
+							<input type="checkbox" name="test3" id="" defaultValue="time" /> Time
+						</label>
+					<input type="submit" value="submit" />
+				</form>
+				
 			<select onChange={handleOption} id="option">
 				<option value="" selected>Select option</option>
 				<option value="New">New</option>
@@ -56,22 +72,24 @@ const Posts = ({ posts }) => {
 				<option value="New">New</option>
 				<option value="17">17</option>
 			</select>
+			
 			</Flex>
-				<Flex>
-					<p style={{textAlign: 'center', width: '100%'}}>{myPosts.length == 0 && 'Nema postova'}</p>
-						{myPosts.map( post => (
-							<Card key={post.id}  id={post.id}>
-								<Link href={ `/blog/${ post.slug }` }>
-									<a className="test" id={post.id}>
-										<div className="bg">
-									      <Image src={post.featured_image.next_post_size} alt={post.title} unsized/>
-									      <p>{post.title}</p>
-										</div>
-									</a>
-								</Link>
-							</Card>
-						))}
-				</Flex>
+			<Flex>
+				<p style={{textAlign: 'center', width: '100%'}}>{myPosts.length == 0 && 'Nema postova'}</p>
+			
+					{myPosts.map( post => (
+						<Card key={post.id}  id={post.id}>
+							<Link href={ `/blog/${ post.slug }` }>
+								<a className="test" id={post.id}>
+									<div className="bg">
+										<Image src={post.featured_image.next_post_size} alt={post.title} unsized/>
+										<p>{post.title}</p>
+									</div>
+								</a>
+							</Link>
+						</Card>
+					))}
+			</Flex>
 
 			</Container>
 			<style global jsx>
@@ -89,18 +107,12 @@ const Posts = ({ posts }) => {
 
 	const res = await fetch(process.env.API)
 	const posts = await res.json()
-	const allPosts = posts.length;
 
 	return {
 	  props: {
-		posts,
-		allPosts
+		posts
 	  },
-	  revalidate: 30
+	   revalidate: 30
 	}
   }
-
   export default Posts
-
-
-  
